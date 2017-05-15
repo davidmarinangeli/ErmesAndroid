1540c024e4a6d9796c0c870fc1bba9743601d441
davidmarinangeli <david.marinangeli@gmail.com>
2017-05-15 17:29:05 +0200

    Risolto un crash nella Main Activity

--- app/src/main/java/com/example/david/ermes/View/activities/MainActivity.java
+++ app/src/main/java/com/example/david/ermes/View/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ import com.example.david.ermes.R;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Toolbar toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,13 +22,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_maps);
                     return true;
                 case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_account);
                     return true;
             }
             return false;
@@ -38,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        toolbar.setTitle(R.string.app_name);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
