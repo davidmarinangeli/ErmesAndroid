e70ed4442c5e6ccf5372df564c8be54ed1b43898
Unknown <david.marinangeli@gmail.com>
2017-12-20 23:23:17 +0100

    Modifiche activity_signup
    progress del lavoro sull'activity della registrazione in attesa di un bug fix degli utenti.

--- app/src/main/java/com/example/david/ermes/View/activities/SignUpActivity.java
+++ app/src/main/java/com/example/david/ermes/View/activities/SignUpActivity.java
@@ -1,10 +1,12 @@
 package com.example.david.ermes.View.activities;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,8 +22,16 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     private FirebaseAuth mAuth;
     private static final String TAG = "SignUpActivity";
-    private EditText mail_editext;
-    private EditText password_editext;
+
+    private Button registrati_button;
+
+    private EditText mail_editText;
+    private EditText password_editText;
+    private EditText city_editText;
+    private EditText username_editText;
+
+    private TextInputLayout mail_til;
+    private TextInputLayout password_til;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +42,16 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         mAuth = FirebaseAuth.getInstance();
 
         // setto i parametri dell'editext login e password
-        mail_editext = findViewById(R.id.mail);
-        password_editext = findViewById(R.id.pwd);
+        mail_editText = findViewById(R.id.mail);
+        password_editText = findViewById(R.id.pwd);
+        city_editText = findViewById(R.id.city_signup);
+        username_editText = findViewById(R.id.username_signup);
+
+        registrati_button = findViewById(R.id.registrati_button);
+        registrati_button.setOnClickListener(this);
+
+        mail_til = findViewById(R.id.textInputLayoutMail);
+        password_til = findViewById(R.id.textInputLayoutPwd);
     }
 
     private void signUpNormal(final String email, final String password){
@@ -58,6 +76,21 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
+        if (view.getId() == R.id.registrati_button){
+            if ((mail_editText.getText() != null) &&
+                    (password_editText.getText() != null) &&
+                    (username_editText.getText() != null) &&
+                    (city_editText.getText() != null))
+            {
+                signUpNormal(mail_editText.getText().toString(), password_editText.getText().toString());
 
+            } else {
+                if (mail_editText.getText() == null)
+                    mail_til.setError("Inserisci una mail");
+                if (password_editText.getText() == null)
+                    password_til.setError("Inserisci una password");
+
+            }
+        }
     }
 }
