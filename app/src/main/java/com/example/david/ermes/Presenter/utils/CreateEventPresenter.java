f977de3460788ea11bbf5d1b2299078998c9306a
Unknown <david.marinangeli@gmail.com>
2018-01-04 23:52:54 +0100

    Nuovo Presenter, nuovi metodi per il database, conclusione sign-up
    Nuovo presenter per Create Event Presenter, conclusione (quasi) della registrazione con Google o Mail e password, correzione al Login con Google, e altro credo...

---
+++ app/src/main/java/com/example/david/ermes/Presenter/utils/CreateEventPresenter.java
@@ -0,0 +1,72 @@
+package com.example.david.ermes.Presenter.utils;
+
+import com.example.david.ermes.Model.db.FirebaseCallback;
+import com.example.david.ermes.Model.models.Location;
+import com.example.david.ermes.Model.models.Match;
+import com.example.david.ermes.Model.models.Sport;
+import com.example.david.ermes.Model.models.User;
+import com.example.david.ermes.Model.repository.SportRepository;
+import com.example.david.ermes.Model.repository.UserRepository;
+import com.example.david.ermes.View.activities.CreateEventActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by david on 1/3/2018.
+ */
+
+public class CreateEventPresenter {
+
+    private CreateEventActivity createEventActivity;
+
+    public CreateEventPresenter(CreateEventActivity createEventActivity){
+        this.createEventActivity = createEventActivity;
+    }
+
+   public void saveMatch(final long timeInMillis, final String sport){
+
+       UserRepository.getInstance().getUser(new FirebaseCallback() {
+           @Override
+           public void callback(Object object) {
+               if (object != null) {
+                   final User current_user = (User) object;
+
+                   final Location location = new Location("Alessandro Volta", 46.0490089, 11.123597, current_user.getUID());
+
+                  SportRepository.getInstance().fetchSportByName(sport, new FirebaseCallback(){
+                      @Override
+                      public void callback(Object object) {
+                          if (object != null){
+                              Sport found_sport = (Sport) object;
+
+                              List<String> missingstuff = new ArrayList<>();
+                              missingstuff.add("rete");
+                              missingstuff.add("pallone");
+
+                              //qui mettere il comportamento alla creazione del match
+                              Match result_match = new Match(
+                                      current_user.getUID(),
+                                      location.getId(),
+                                      com.example.david.ermes.Presenter.utils.TimeUtils.fromMillisToDate(timeInMillis),
+                                      true,
+                                      found_sport.getID(),
+                                      found_sport.getNumPlayers(),
+                                      2,
+                                      missingstuff
+                              );
+
+                              // result_match.setOwner(current_user);
+                              result_match.save();
+
+                              createEventActivity.goToMainActivity(result_match);
+                          }
+                      }
+                  });
+               }
+           }
+       });
+   }
+}
