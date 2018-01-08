c3f028a80e9b015e18c837d88e69c7447e4789ce
Unknown <david.marinangeli@gmail.com>
2018-01-09 00:25:12 +0100

    Nuova classe LocationUtils e cambiamenti alla Creazione dell'evento
    Nuova classe LocationUtils per avere una conversione tra Android Location e Ermes Location. Continuazione dei lavori per la creazione dell'evento nella parte di location.

---
+++ app/src/main/java/com/example/david/ermes/Presenter/utils/LocationUtils.java
@@ -0,0 +1,18 @@
+package com.example.david.ermes.Presenter.utils;
+
+import android.location.Location;
+
+/**
+ * Created by david on 1/9/2018.
+ */
+
+public class LocationUtils {
+
+    public static com.example.david.ermes.Model.models.Location fromAndroidLocationtoErmesLocation(Location androidlocation){
+        com.example.david.ermes.Model.models.Location ermeslocation = new com.example.david.ermes.Model.models.Location();
+        ermeslocation.setLatitude(androidlocation.getLatitude());
+        ermeslocation.setLongitude(androidlocation.getLongitude());
+
+        return ermeslocation;
+    }
+}
