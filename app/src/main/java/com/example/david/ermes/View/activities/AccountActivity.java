1d524c625b2a05c3790360a13b4a0e0ba5c72d9d
Nicola <buretta.nicola@gmail.com>
2018-01-16 11:08:13 +0100

    Aggiunti activity xml, java e recyclerview per la lista di amici

--- app/src/main/java/com/example/david/ermes/View/activities/AccountActivity.java
+++ app/src/main/java/com/example/david/ermes/View/activities/AccountActivity.java
@@ -3,6 +3,7 @@ package com.example.david.ermes.View.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,5 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-
-
     }
 }
--- app/src/main/java/com/example/david/ermes/View/fragments/AccountFragment.java
+++ app/src/main/java/com/example/david/ermes/View/fragments/AccountFragment.java
@@ -5,12 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.david.ermes.Model.db.FirebaseCallback;
+import com.example.david.ermes.Model.models.User;
+import com.example.david.ermes.Model.repository.UserRepository;
 import com.example.david.ermes.R;
+import com.example.david.ermes.View.activities.FriendsActivity;
 import com.example.david.ermes.View.activities.MainSignInActivity;
 
 /**
@@ -21,6 +27,9 @@ public class AccountFragment extends Fragment {
     Button loginbutton;
     Button logoutbutton;
 
+    private CardView friendsCard;
+
+    private User currentUser;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -29,7 +38,6 @@ public class AccountFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -43,11 +51,17 @@ public class AccountFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        UserRepository.getInstance().getUser(new FirebaseCallback() {
+            @Override
+            public void callback(Object object) {
+                currentUser = (User) object;
+            }
+        });
 
         loginbutton = view.findViewById(R.id.loginbutton);
         logoutbutton = view.findViewById(R.id.logoutbutton);
-
-
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,7 +70,23 @@ public class AccountFragment extends Fragment {
 
             }
         });
-        super.onViewCreated(view, savedInstanceState);
+
+        friendsCard = view.findViewById(R.id.cardViewFriends);
+        friendsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentUser != null) {
+                    Bundle extras = new Bundle();
+                    extras.putParcelable("user", currentUser);
+
+                    Intent friendsActivity = new Intent(view.getContext(), FriendsActivity.class);
+                    friendsActivity.putExtras(extras);
+                    startActivity(friendsActivity);
+                } else {
+                    Toast.makeText(view.getContext(), "Attendi...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
