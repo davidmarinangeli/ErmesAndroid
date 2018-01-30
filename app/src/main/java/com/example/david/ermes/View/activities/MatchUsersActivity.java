b01ef06efac8e4aef803825eb856ede2360b1e70
Nicola <buretta.nicola@gmail.com>
2018-01-30 03:23:21 +0100

    Aggiunta la creazione automatica di due squadre con i partecipanti delle partite

--- app/src/main/java/com/example/david/ermes/View/activities/MatchUsersActivity.java
+++ app/src/main/java/com/example/david/ermes/View/activities/MatchUsersActivity.java
@@ -1,12 +1,17 @@
 package com.example.david.ermes.View.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.david.ermes.Model.models.User;
@@ -23,24 +28,38 @@ import java.util.List;
 
 public class MatchUsersActivity extends AppCompatActivity {
 
+    public static final String ACTIVITY_TYPE_KEY = "activity_type";
+    public static final String PARTECIPANTS_TYPE = "PARTECIPANTS_TYPE";
+    public static final String INVITED_TYPES = "INVITED_TYPES";
+
+    private LinearLayout container;
+
     private Toolbar toolbar;
     private UserListAdapter adapter;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private TextView no_users_label;
+    private Button create_teams_btn;
 
     private List<String> userIdList;
-    private String title;
     private User currentUser;
+    private String activity_type;
+
+    private Integer userCount;
+    private Boolean fetchData;
+    private boolean already_show_odd_partecipant_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
 
+        container = findViewById(R.id.activity_friends_container);
+
         adapter = new UserListAdapter(this);
         layoutManager = new LinearLayoutManager(this);
         no_users_label = findViewById(R.id.no_friends_label);
+        create_teams_btn = findViewById(R.id.create_random_teams_button);
 
         recyclerView = findViewById(R.id.friends_recycler_view);
         recyclerView.setAdapter(adapter);
@@ -53,15 +72,33 @@ public class MatchUsersActivity extends AppCompatActivity {
 
         currentUser = getIntent().getExtras().getParcelable("user");
         userIdList = getIntent().getExtras().getStringArrayList("users");
-        title = getIntent().getExtras().getString("title", "Title");
+        activity_type = getIntent().getExtras().getString(ACTIVITY_TYPE_KEY, "type");
+        already_show_odd_partecipant_dialog = false;
 
         adapter.setCurrentUser(currentUser);
-        adapter.refreshUserList(userIdList, null);
+        adapter.refreshUserList(userIdList, object -> {
+            userCount = (Integer) object;
+            fetchData = userCount != null && userCount > 0;
+        });
 
         toolbar = findViewById(R.id.friends_toolbar);
-        toolbar.setTitle(title + " (" + userIdList.size() + ")");
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setContent();
+    }
+
+    private void setContent() {
+        switch (activity_type) {
+            case PARTECIPANTS_TYPE:
+                toolbar.setTitle("Partecipanti (" + userIdList.size() + ")");
+
+                create_teams_btn.setVisibility(View.VISIBLE);
+                create_teams_btn.setOnClickListener(v -> createRandomTeams());
+                break;
+            case INVITED_TYPES:
+                toolbar.setTitle("Invitati (" + userIdList.size() + ")");
+                create_teams_btn.setVisibility(View.GONE);
+                break;
+        }
 
         if (userIdList.size() > 0) {
             no_users_label.setVisibility(View.GONE);
@@ -69,19 +106,32 @@ public class MatchUsersActivity extends AppCompatActivity {
             no_users_label.setText("Nessun utente");
             no_users_label.setVisibility(View.VISIBLE);
         }
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private int fetch_users_count = 0;
-    private void incrementFetchUsersCount() {
-        fetch_users_count++;
-    }
+    private void createRandomTeams() {
+        if (fetchData) {
+            if (userCount < 2) {
+                Snackbar.make(container, "Devono esserci almeno 2 partecipanti per creare le squadre.",
+                        Snackbar.LENGTH_LONG).show();
+            } else if (userCount % 2 != 0 && !already_show_odd_partecipant_dialog) {
+                already_show_odd_partecipant_dialog = true;
 
-    private int getFetchUsersCount() {
-        return fetch_users_count;
-    }
-
-    private void resetFetchUsersCount() {
-        fetch_users_count = 0;
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("Il numero dei partecipanti è dispari, la squadra 2 avrà " +
+                                "un giocatore in più della squadra 1.")
+                        .setPositiveButton("OK", ((dialog, which) -> adapter.createRandomTeams()))
+                        .show();
+            } else {
+                adapter.createRandomTeams();
+            }
+        } else {
+            Snackbar.make(container, "Attendi lo scaricamento dei dati, riprova tra poco.",
+                    Snackbar.LENGTH_LONG).show();
+        }
     }
 
     @Override
